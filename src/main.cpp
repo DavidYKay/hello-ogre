90c833465883a4ce10ee0c752fb1e3c2ff2bca65
David Young-chan Kay <davidykay@gmail.com>
2018-12-14 00:01:32 -0800

    Trying a more minimal example, but not successfully loading the dynamic libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,115 +1,58 @@
 #include <Ogre.h>
-#include <Overlay/OgreTextAreaOverlayElement.h>
-#include <Overlay/OgreFontManager.h>
-#include "ExampleFrameListener.h"
-#define FONT_FILE_NAME "solo5.ttf"
-// save some horizontal space
 using namespace Ogre;
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-#define FONT_FOLDER "C:\\My\\Font\\Folder"
-#define WIN32_LEAN_AND_MEAN
-#include "windows.h"
-INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
-#else
-#define FONT_FOLDER "/usr/share/ogrenew/Samples/Media/fonts"
-int main(int argc, char **argv)
-#endif
-{
-  // create root
-  Root *root = new Root();
-  // choose renderer
-  if(!root->showConfigDialog())
-  {
-    return 0;
+
+void loadRenderSystems() {
+  Root* ogreRoot = Root::getSingletonPtr();
+
+  try {
+    ogreRoot->loadPlugin("RenderSystem_GL");
   }
-  // initialise root
-  root->initialise(false);
-  // create main window
-  RenderWindow *renderWindow = root->createRenderWindow("Main",320,240,false);
-  // create the scene
-  SceneManager *sceneMgr = root->createSceneManager(Ogre::ST_GENERIC);
-  // add a camera
-  Camera *mainCam = sceneMgr->createCamera("MainCam");
-  // add viewport
-  Viewport *vp = renderWindow->addViewport(mainCam);
+  catch(Exception& e) {
+    LogManager::getSingleton().logMessage(String("Unable to create OpenGL RenderSystem: ") + e.getFullDescription());
+  }
 
+//#if defined(_DEBUG)
+//    ogre->loadPlugin("Plugin_CgProgramManager_d");
+//    ogre->loadPlugin("Plugin_OctreeSceneManager_d");
+//#else
+//    ogre->loadPlugin("Plugin_CgProgramManager");
+//    ogre->loadPlugin("Plugin_OctreeSceneManager");
+//#endif
 
+  try {
+    ogreRoot->loadPlugin("Plugin_CgProgramManager");
+  }
+  catch(Exception& e) {
+    LogManager::getSingleton().logMessage(String("Unable to create CG Program manager RenderSystem: ") + e.getFullDescription());
+  }
 
-  // Create a font
-  //
-  //
-
-  // get the resource manager
-  ResourceGroupManager &resGroupMgr = ResourceGroupManager::getSingleton();
-  // tell it to look at this location
-  resGroupMgr.addResourceLocation(FONT_FOLDER, "FileSystem");
-  // get the font manager
-  FontManager &fontMgr = FontManager::getSingleton();
-  // create a font resource
-  ResourcePtr font = fontMgr.create("MyFont","General");
-  // set as truetype
-  font->setParameter("type","truetype");
-  // set the .ttf file name
-  font->setParameter("source",FONT_FILE_NAME);
-  // set the size
-  font->setParameter("size","26");
-  // set the dpi
-  font->setParameter("resolution","96");
-  // load the ttf
-  font->load();
-
-
-  // Create the TextAreaOverlayElement
-
-  // get the overlay manager
-  OverlayManager& overlayMgr = OverlayManager::getSingleton();
-
-  Here we create a panel and set its position and size.
-
-    // Create a panel
-    Ogre::OverlayContainer* panel = static_cast<OverlayContainer*>(
-        overlayMgr.createOverlayElement("Panel", "PanelName"));
-  panel->setMetricsMode(Ogre::GMM_PIXELS);
-  panel->setPosition(10, 10);
-  panel->setDimensions(300, 120);
-
-  // Create a text area
-  TextAreaOverlayElement* textArea = static_cast<TextAreaOverlayElement*>(
-      overlayMgr.createOverlayElement("TextArea", "TextAreaName"));
-  textArea->setMetricsMode(Ogre::GMM_PIXELS);
-  textArea->setPosition(0, 0);
-  textArea->setDimensions(300, 120);
-  textArea->setCharHeight(26);
-  // set the font name to the font resource that you just created.
-  textArea->setFontName("MyFont");
-  // say something
-  textArea->setCaption("Hello, World!");
-
-  // Here we create an Overlay to display the panel
-
-  // Create an overlay, and add the panel
-  Ogre::Overlay* overlay = overlayMgr.create("OverlayName");
-  overlay->add2D(panel);
-
-  // We have to add the text area to the panel _after_ adding the panel to an overlay to avoid a segfault.
-
-  // Add the text area to the panel
-  panel->addChild(textArea);
-
-  // And display our overlay.
-
-  // Show the overlay
-  overlay->show();
-
-  // setup frame listener
-  ExampleFrameListener *frameListener= new ExampleFrameListener(renderWindow, mainCam);
-  frameListener->showDebugOverlay(false);
-  root->addFrameListener(frameListener);
-
-  // start rendering
-  root->startRendering();
-
-  return 0;
+  Ogre::RenderSystemList renderSystems = ogreRoot->getAvailableRenderers();
+  Ogre::RenderSystemList::const_iterator r_it = renderSystems.begin();
+  ogreRoot->setRenderSystem(*r_it);
 }
+//'const RenderSystemList  {aka const std::vector<Ogre::RenderSystem*, Ogre::STLAllocator<Ogre::RenderSystem*, Ogre::CategorisedAllocPolicy<(Ogre::MemoryCategory)0> > >}'
+//'Ogre::RenderSystemList* {aka       std::vector<Ogre::RenderSystem*, Ogre::STLAllocator<Ogre::RenderSystem*, Ogre::CategorisedAllocPolicy<(Ogre::MemoryCategory)0> > >*}'
+//in assignment
 
+int main()
+{
+    Root *root = new Root("plugins.cfg", "mygame.cfg", "mygame.log");
+    //root->showConfigDialog();
+
+    loadRenderSystems();
+
+    root->initialise(true, "My Game");
+    RenderWindow *window = root->getAutoCreatedWindow();
+
+    SceneManager *smgr = root->createSceneManager(ST_GENERIC, "SceneManager");
+
+    Camera *cam = smgr->createCamera("MainCamera");
+
+    Viewport *vp = window->addViewport(cam);
+    vp->setBackgroundColour(ColourValue(0.3, 0.6, 0.9));
+
+    root->startRendering();
+
+    return 0;
+}
 
