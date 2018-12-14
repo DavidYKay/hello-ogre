28a87f24699faf76bc17b89247f43265b3372514
David Young-chan Kay <davidykay@gmail.com>
2018-12-14 13:01:47 -0800

    Got background color to render as sky blue

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,51 @@
 #include <Ogre.h>
+#include <OgreColourValue.h>
 
 int main(int argc, char *argv[])
 {
-	// the root
-	Ogre::Root *root = new Ogre::Root("", "");
+  // the root
+  Ogre::Root *root = new Ogre::Root("", "");
 
-	// load the rendersystem
+  // load the rendersystem
   root->loadPlugin("/usr/lib64/OGRE/RenderSystem_GL");
-	root->setRenderSystem(*(root->getAvailableRenderers().begin()));
-	root->initialise(false);
+  root->setRenderSystem(*(root->getAvailableRenderers().begin()));
+  root->initialise(false);
 
-	// the window
-	Ogre::RenderWindow *window = root->createRenderWindow("Hello World!", 800, 600, false);
-	window->setActive(true);
-	window->setAutoUpdated(true);
-	window->setDeactivateOnFocusChange(false);
+  // the window
+  Ogre::RenderWindow *window = root->createRenderWindow("Hello World!", 800, 600, false);
+  window->setActive(true);
+  window->setAutoUpdated(true);
+  window->setDeactivateOnFocusChange(false);
 
-	Ogre::SceneManager *sceneMgr = root->createSceneManager(Ogre::ST_GENERIC);
+  Ogre::SceneManager *sceneMgr = root->createSceneManager(Ogre::ST_GENERIC);
 
-	// viewport and camera
-	Ogre::Camera *camera = sceneMgr->createCamera("cam");
-	Ogre::Viewport *viewport = window->addViewport(camera);
-	viewport->setClearEveryFrame(true);
-	// TODO: set-up your camera
+  // viewport and camera
+  Ogre::Camera *camera = sceneMgr->createCamera("cam");
+  Ogre::Viewport *viewport = window->addViewport(camera);
+  viewport->setClearEveryFrame(true);
+  viewport->setBackgroundColour(Ogre::ColourValue(0.3, 0.6, 0.9));
 
-	// TODO: set-up your resources
+  // TODO: set-up your camera
 
-	// TODO: set-up your lighting
+  // TODO: set-up your resources
 
-	// TODO: create your objects
+  // TODO: set-up your lighting
 
-	// main loop
-	while (true)
-	{
-		// TODO: do your game logic here
 
-		Ogre::WindowEventUtilities::messagePump();
+  // main loop
+  while (true) {
+    // TODO: do your game logic here
 
-		if (!root->renderOneFrame())
-			break;
-	}
+    Ogre::WindowEventUtilities::messagePump();
 
-	// clean up
-	delete root;
+    if (!root->renderOneFrame())
+      break;
+  }
 
-	// the end.
-	return 0;
+  // clean up
+  delete root;
+
+  // the end.
+  return 0;
 }
 
