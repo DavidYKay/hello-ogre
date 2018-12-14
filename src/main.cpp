16d5467fbe1a8fb7fcde5f93172ce6546d7e6d04
David Young-chan Kay <davidykay@gmail.com>
2018-12-14 13:05:40 -0800

    adding code for rendering a cube

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,42 @@
 #include <Ogre.h>
 #include <OgreColourValue.h>
 
+ManualObject* createCubeMesh(Ogre::String name, Ogre::String matName) {
+  ManualObject* cube = new ManualObject(name);
+  cube->begin(matName);
+
+   cube-> position( 0.5,-0.5,1.0);  cube-> normal(0.408248,-0.816497,0.408248);   cube-> textureCoord(1,0);
+   cube-> position(-0.5,-0.5,0.0); cube-> normal(-0.408248,-0.816497,-0.408248); cube-> textureCoord(0,1);
+   cube-> position( 0.5,-0.5,0.0);  cube-> normal(0.666667,-0.333333,-0.666667);  cube-> textureCoord(1,1);
+   cube-> position(-0.5,-0.5,1.0); cube-> normal(-0.666667,-0.333333,0.666667);  cube-> textureCoord(0,0);
+   cube-> position( 0.5,0.5,1.0);   cube-> normal(0.666667,0.333333,0.666667);    cube-> textureCoord(1,0);
+   cube-> position(-0.5,-0.5,1.0); cube-> normal(-0.666667,-0.333333,0.666667);  cube-> textureCoord(0,1);
+   cube-> position( 0.5,-0.5,1.0);  cube-> normal(0.408248,-0.816497,0.408248);   cube-> textureCoord(1,1);
+   cube-> position(-0.5,0.5,1.0);  cube-> normal(-0.408248,0.816497,0.408248);   cube-> textureCoord(0,0);
+   cube-> position(-0.5,0.5,0.0);  cube-> normal(-0.666667,0.333333,-0.666667);  cube-> textureCoord(0,1);
+   cube-> position(-0.5,-0.5,0.0); cube-> normal(-0.408248,-0.816497,-0.408248); cube-> textureCoord(1,1);
+   cube-> position(-0.5,-0.5,1.0); cube-> normal(-0.666667,-0.333333,0.666667);  cube-> textureCoord(1,0);
+   cube-> position( 0.5,-0.5,0.0);  cube-> normal(0.666667,-0.333333,-0.666667);  cube-> textureCoord(0,1);
+   cube-> position( 0.5,0.5,0.0);   cube-> normal(0.408248,0.816497,-0.408248);   cube-> textureCoord(1,1);
+   cube-> position( 0.5,-0.5,1.0);  cube-> normal(0.408248,-0.816497,0.408248);   cube-> textureCoord(0,0);
+   cube-> position( 0.5,-0.5,0.0);  cube-> normal(0.666667,-0.333333,-0.666667);  cube-> textureCoord(1,0);
+   cube-> position(-0.5,-0.5,0.0); cube-> normal(-0.408248,-0.816497,-0.408248); cube-> textureCoord(0,0);
+   cube-> position(-0.5,0.5,1.0);  cube-> normal(-0.408248,0.816497,0.408248);   cube-> textureCoord(1,0);
+   cube-> position( 0.5,0.5,0.0);   cube-> normal(0.408248,0.816497,-0.408248);   cube-> textureCoord(0,1);
+   cube-> position(-0.5,0.5,0.0);  cube-> normal(-0.666667,0.333333,-0.666667);  cube-> textureCoord(1,1);
+   cube-> position( 0.5,0.5,1.0);   cube-> normal(0.666667,0.333333,0.666667);    cube-> textureCoord(0,0);
+
+  cube->triangle(0,1,2);    cube->triangle(3,1,0);
+  cube->triangle(4,5,6);    cube->triangle(4,7,5);
+  cube->triangle(8,9,10);   cube->triangle(10,7,8);
+  cube->triangle(4,11,12);  cube->triangle(4,13,11);
+  cube->triangle(14,8,12);  cube->triangle(14,15,8);
+  cube->triangle(16,17,18); cube->triangle(16,19,17);
+  cube->end();
+
+  return cube;
+}
+
 int main(int argc, char *argv[])
 {
   // the root
@@ -28,6 +64,10 @@ int main(int argc, char *argv[])
   // TODO: set-up your camera
 
   // TODO: set-up your resources
+
+  SceneNode* mNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+  mNode->setPosition(0,0.5,0);
+  mNode->attachObject(createCubeMesh("Cube", "myMaterial"));
 
   // TODO: set-up your lighting
 
